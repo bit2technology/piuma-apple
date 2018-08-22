b95d01d6424ad47ac7d5b21010b661b68e76b8fe
Igor Camilo <igor.rcamilo@gmail.com>
2018-08-22 19:19:47 -0300

    Add macOS app.

---
+++ Piuma-macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Piuma-macOS
+//
+//  Created by Igor Camilo on 22/08/18.
+//  Copyright © 2018 Bit2 Technology. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Piuma-macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Piuma-macOS
+//
+//  Created by Igor Camilo on 22/08/18.
+//  Copyright © 2018 Bit2 Technology. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
