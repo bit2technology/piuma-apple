ba5c7d71b4522028ac5945148457278399d555af
Igor Camilo <igor.rcamilo@gmail.com>
2018-08-22 18:34:51 -0300

    Add iOS framework.

---
+++ PiumaCore-iOS/PiumaCore_iOS.h
@@ -0,0 +1,19 @@
+//
+//  PiumaCore_iOS.h
+//  PiumaCore-iOS
+//
+//  Created by Igor Camilo on 22/08/18.
+//  Copyright © 2018 Bit2 Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PiumaCore_iOS.
+FOUNDATION_EXPORT double PiumaCore_iOSVersionNumber;
+
+//! Project version string for PiumaCore_iOS.
+FOUNDATION_EXPORT const unsigned char PiumaCore_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PiumaCore_iOS/PublicHeader.h>
+
+
